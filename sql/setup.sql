203d8401ee633645d1706b3d7973642ccfcf8791
Katie <katieberrym@gmail.com>
2020-12-09 14:16:36 -0800

    initial setup

---
+++ sql/setup.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS alpaca_walkers CASCADE;
+DROP TABLE IF EXISTS alpacas;
+
+
+CREATE TABLE alpaca_walkers (
+
+);
+
+//alpacas will reference our walkers
+CREATE TABLE alpacas (
+
+
+);
