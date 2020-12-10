26e2d430263434778449914f375d47c89aba5f4a
Katie <katieberrym@gmail.com>
2020-12-09 19:31:20 -0800

    added delete  route for walkers and related test - test pass

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -93,5 +93,18 @@ describe('app routes', () => {
             yearsOfExperience: 2
         });
     });
+
+    it('deletes one alpaca_walkers by ID via DELETE, and returns it', async () => {
+        const alpaca_walker = await Alpaca_walker.insert({
+            name: 'Marco',
+            energyLevel: 'upbeat',
+            yearsOfExperience: 2
+        });
+
+        const res = await request(app)
+            .delete(`/alpaca-walkers/${alpaca_walker.id}`);
+
+        expect(res.body).toEqual(alpaca_walker);
+    });
 });
 
